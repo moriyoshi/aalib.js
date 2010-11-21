4559c51ff5371799f4acf365aae13ebf4d7cd44d
Moriyoshi Koizumi <mozo@mozo.jp>
2010-11-22 08:18:24 +0900

    Add license.

--- aalib.js
+++ aalib.js
@@ -1,3 +1,23 @@
+// aalib.js - A JavaScript port of AALib 1.2
+//
+// Copyright (C) 2001 Jan Hubicka, Thomas A. K. Kjaer, Tim Newsome and
+//                    Kamil Toman.
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+
 var AALib = function(params) {
     this.font = params.font || AALib.fonts.font16;
     this.supported = params.supported || AALib.NORMAL_MASK | AALib.DIM_MASK | AALib.BOLD_MASK;
